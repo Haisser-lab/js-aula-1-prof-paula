bff4913483bb7d4f22999756817dd76ffdca3738
Haisser-lab <84329660+Haisser-lab@users.noreply.github.com>
2023-03-14 11:22:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log('Olá Mundo!')
+
+var idade = 17
+
+var nome = "Bernardo" 
+
+var sobrenome = "Haisser"
+
+var estudante = true
+if (idade === 17) {
+    console.log(nome, sobrenome, "tem 17 anos")
+    if (estudante == true) {
+        console.log(nome, sobrenome, "é estudante")
+    }
+    else{
+        console.log(nome, sobrenome, "não é estudante")
+    }
+}
+else{
+    console.log(nome, sobrenome,"não tem 17 anos")
+}
